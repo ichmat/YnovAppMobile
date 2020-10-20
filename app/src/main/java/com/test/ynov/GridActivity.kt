c23d2d59565988920ea37374466d6e3147f09e8c
mattéo lopes <ichmatteo05@gmail.com>
2020-10-20 13:29:30 +0200

    Utilisation d'un gridlayout et d'un scrollview

---
+++ app/src/main/java/com/test/ynov/GridActivity.kt
@@ -0,0 +1,14 @@
+package com.test.ynov
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Html
+import android.widget.NumberPicker
+import androidx.appcompat.app.ActionBar
+
+class GridActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.scrollview)
+    }
+}
